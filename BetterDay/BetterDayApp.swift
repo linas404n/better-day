b36ec4ba921c2983fd2c458f80caa4fa93131bf8
Linas <linasn404@gmail.com>
2024-11-08 22:53:19 +0100

    - Create project

---
+++ BetterDay/BetterDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterDayApp.swift
+//  BetterDay
+//
+//  Created by Linas on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
